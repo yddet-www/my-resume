1d8716f9afdec3886416363a0d14fcd5a71a4a03
yddet-www <yddet.www@gmail.com>
2025-08-20 18:12:00 -0500

    Fixing MIME type error
    - Fixing disallowed MIME

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "https://yddet-www.github.io/my-resume/",
 });
